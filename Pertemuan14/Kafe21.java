962902eb02af679f44389f650191991037424b4d
ratnawahyuni12 <ratna120307@gmail.com>
2025-12-02 23:40:07 +0700

    Percobaan 2 : Membuat Fungsi Dengan Parameter

--- Pertemuan14/Kafe21.java
+++ Pertemuan14/Kafe21.java
@@ -2,7 +2,13 @@ package Pertemuan14;
 
 public class Kafe21 {
     
-    public static void Menu() {
+    public static void Menu(String namaPelanggan, boolean isMember) {
+        System.out.println("Selamat datang, " + namaPelanggan + "!");
+
+        if (isMember) {
+            System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
+        }
+
         System.out.println("===== MENU RESTO KAFE =====");
         System.out.println("1. Kopi Hitam - Rp 15,000");
         System.out.println("2. Cappuccino - Rp 20,000");
@@ -13,16 +19,7 @@ public class Kafe21 {
         System.out.println("===========================");
         System.out.println("Silakan pilih menu yang Anda inginkan.");
     }
-
     public static void main(String[] args) {
-        System.out.println("===== MENU RESTO KAFE =====");
-        System.out.println("1. Kopi Hitam - Rp 15,000");
-        System.out.println("2. Cappuccino - Rp 20,000");
-        System.out.println("3. Latte - Rp 22,000");
-        System.out.println("4. Teh Tarik - Rp 12,000");
-        System.out.println("5. Roti Bakar - Rp 10,000");
-        System.out.println("6. Mie Goreng - rp 18,000");
-        System.out.println("===========================");
-        System.out.println("Silakan pilih menu yang Anda inginkan.");
+        Menu("Andi", true);
     }
 }
