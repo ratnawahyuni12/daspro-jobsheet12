6341242d7a92fd4390851a72cc9f2cbbb796cf9a
ratnawahyuni12 <ratna120307@gmail.com>
2025-12-03 07:47:33 +0700

    Percobaan 3 : Membuat Fungsi dengan Nilai Kembalian

--- Pertemuan14/Kafe21.java
+++ Pertemuan14/Kafe21.java
@@ -1,5 +1,5 @@
 package Pertemuan14;
-
+import java.util.Scanner;
 public class Kafe21 {
     
     public static void Menu(String namaPelanggan, boolean isMember, String kodePromo) {
@@ -23,11 +23,31 @@ public class Kafe21 {
         System.out.println("3. Latte - Rp 22,000");
         System.out.println("4. Teh Tarik - Rp 12,000");
         System.out.println("5. Roti Bakar - Rp 10,000");
-        System.out.println("6. Mie Goreng - rp 18,000");
+        System.out.println("6. Mie Goreng - Rp 18,000");
         System.out.println("===========================");
         System.out.println("Silakan pilih menu yang Anda inginkan.");
     }
+
+    public static int hitungTotalHarga21(int pilihanMenu, int banyakItem) {
+        int[] hargaItems = {15000, 20000, 22000, 12000, 10000, 18000};
+
+        int hargaTotal = hargaItems[pilihanMenu-1] * banyakItem;
+        return hargaTotal;
+    }
+
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         Menu("Andi", true, "DISKON50");
+
+        System.out.print("\nMasukkan nomor menu yang ingin Anda pesan: ");
+        int pilihanMenu = sc.nextInt();
+        System.out.print("Masukkan jumlah item yang ingin dipesan: ");
+        int banyakItem = sc.nextInt();
+
+        int totalHarga = hitungTotalHarga21(pilihanMenu, banyakItem);
+
+        System.out.println("Total harga untuk pesanan Anda: Rp " + totalHarga);
+
+        sc.close();
     }
 }
