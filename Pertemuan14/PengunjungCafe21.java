d3ae050f769a9c837d870034d08ab8cdb29c05ae
ratnawahyuni12 <ratna120307@gmail.com>
2025-12-03 16:37:53 +0700

    Modifikasi percobaan 4 (perintah nomor 2)

--- Pertemuan14/PengunjungCafe21.java
+++ Pertemuan14/PengunjungCafe21.java
@@ -4,8 +4,8 @@ public class PengunjungCafe21 {
     
     static void daftarPengunjung(String... namaPengunjung) {
         System.out.println("Daftar Nama Pengunjung:");
-        for (int i = 0; i < namaPengunjung.length; i++) {
-            System.out.println("- " + namaPengunjung[i]);
+        for (String nama : namaPengunjung) {
+            System.out.println(("- " + nama));
         }
     }
 
